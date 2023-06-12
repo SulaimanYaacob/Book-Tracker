7a9404c2c9ce55cc07bcdac81384b5a9a825887b
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-06-12 18:21:58 +0800

    update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import '../Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -14,9 +18,20 @@ function Home() {
         <p id='value'>amin haiqal</p>
         <p id='desc'>Don't let your love for books fade away! Rediscover the enhancement by tracking your favourite read. Let's keep the literacy going.</p>
       </div>
+
+      <div id="button" className="btn btn-dark d-flex align-items-center justify-content-center"><p>Continue Reading</p></div>
+
+      {/* Task Today: 12 June 2023 */}
       <div id='book'>
-        <img src='../src/assets/images/SurroundedBySetbacks.jpg' alt='book' />
+        <div id='book-image'><img src='../src/assets/images/SurroundedBySetbacks.jpg' alt='book' /></div>
+        <div id='book-details'>
+          <div id='book-title'><p>Surrounded by Setbacks: Or, How to Succeed When Everything's Gone Bad</p></div>
+          <div id='book-page'><p id='current-page'>236</p><p>/</p><p id='total-page'>317</p><p>pages</p></div>
+        </div>
       </div>
+
+
+
     </div>
 
 
