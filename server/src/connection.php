bd895391056253a3bcf8048920728db420129d46
Sulaiman Yaacob <davidjethro01@gmail.com>
2023-06-23 18:38:15 +0800

    added my_list_books

---
+++ server/src/connection.php
@@ -0,0 +1,38 @@
+<?php 
+
+    class database {
+
+        //db credentials
+        private $host = "localhost";  
+        private $username = "root";
+        private $password = "";
+        private $db_name = "book_tracker";
+        
+        // Create connection
+        public function connection() {
+            $mysql_connect_str = "mysql:host=$this->host;dbname=$this->db_name";
+            $dbConnection = new PDO($mysql_connect_str, $this->username, $this->password);
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            return $dbConnection;
+        }
+
+        public function createTable() {
+            //Create table user for id name and email
+            $sql = "CREATE TABLE IF NOT EXISTS my_list_books (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                userId BINARY(16) NOT NULL
+            )";
+
+            $stmt = $this->connection()->prepare($sql);
+            $stmt->execute();
+        }
+        
+    }
+
+    // Create a database object
+    $database = new database();
+    $database->connection();
+    $database->createTable();
+    
+?>
--- server/src/list_books.php
+++ server/src/list_books.php
@@ -25,12 +25,12 @@ class CorsMiddleware
 
 $app->add(new CorsMiddleware());
 
-// Get Single User
-$app->get('/api/user/{id}', function(Request $request, Response $response, $args) use ($db) {
-    $id = $args['id'];
-    $sql = "SELECT * FROM user WHERE id = :id";
+// Get List of my Books
+$app->get('/api/user/{userId}', function(Request $request, Response $response, $args) use ($db) {
+    $userId = $args['userId'];
+    $sql = "SELECT * FROM my_list_books WHERE userId = :userId";
     $stmt = $db->prepare($sql);
-    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':userId', $userId);
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_OBJ);
     return $response->withJson($result);
