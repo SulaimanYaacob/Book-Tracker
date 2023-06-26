a1a0d9c335887f8cac9e3490a06295aa019b4a47
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-06-26 09:48:44 +0800

    update homepage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BookSearch from "./pages/BookSearch.tsx";
-import Home from "./pages/Home.tsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import BookDetails from "./pages/BookDetails.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
 //TODO Add other pages. add path & element props
 
@@ -13,9 +13,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
         <Route path="/search" element={<BookSearch />} />
-        <Route path="/" />
+        <Route path="/" element={<HomePage/>}/>
         <Route path="/" />
         <Route path="*" element={<h1>Error 404</h1>} />
         <Route path="/book/:id" element={<BookDetails />} />
